7df0bf7e5d28fa1e979e0757063882d8793eb3ff
Vadim <99843783+NevermindGit@users.noreply.github.com>
2022-10-01 01:42:36 +0300

    Update Contents.swift

--- 102/Layers.playground/Contents.swift
+++ 102/Layers.playground/Contents.swift
@@ -1,38 +1,58 @@
-import UIKit
+mport UIKit
 import PlaygroundSupport
-
-let size = CGSize(width: 100, height: 100)
+// PlaygroundPage.current.needsIndefiniteExecution = true
+let size100 = CGSize(width: 100, height: 100)
 let view = UIView()
-//(238,130,238)
-view.backgroundColor = UIColor(red: 238/255.0, green: 130/255.0, blue: 238/255.0, alpha: 1)
-view.frame = CGRect(origin: .zero, size: CGSize(width: 600, height: 400))
+view.backgroundColor = .blue
+view.frame = CGRect(origin: .zero, size: CGSize(width: 300, height: 600))
 PlaygroundPage.current.liveView = view
-
+ 
 let layer = view.layer
-
+let squareLayer = CALayer()
+squareLayer.frame = CGRect(
+    origin: CGPoint(x: 20, y: 120),
+    size: size100
+)
+squareLayer.backgroundColor = UIColor.red.cgColor
+layer.addSublayer(squareLayer)
+ 
+let circleLayer = CALayer()
+circleLayer.backgroundColor = UIColor.gray.cgColor
+circleLayer.frame = CGRect(
+    origin: .zero, size: size100
+)
+circleLayer.cornerRadius = 50
+layer.addSublayer(circleLayer)
+circleLayer.frame.origin = CGPoint(
+    x: view.center.x
+        - circleLayer.frame.width * 0.5,
+    y: view.center.y
+        - circleLayer.frame.height * 0.5
+)
+ 
 let gradientLayer = CAGradientLayer()
 gradientLayer.startPoint = .zero
-gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+gradientLayer.endPoint = CGPoint(x: 1, y: 1)
 gradientLayer.colors = [
-    UIColor(red: 128/255.0, green: 0, blue: 128/255.0, alpha: 0.5).cgColor,
-    UIColor(red: 0, green: 0, blue: 1, alpha: 0.5).cgColor
+    UIColor.green.cgColor.copy(alpha: 0.5) as Any,
+    UIColor.purple.cgColor.copy(alpha: 0.5) as Any,
+    UIColor.cyan.cgColor.copy(alpha: 0.5) as Any,
 ]
 gradientLayer.frame = layer.bounds
 layer.addSublayer(gradientLayer)
-
-let borderLayer = CALayer()
-borderLayer.bounds = CGRect(x: 0, y: 0, width: 600, height: 400)
-borderLayer.position = CGPoint(x: 600/2, y: 400/2)
-borderLayer.backgroundColor = UIColor(red: 128/255.0, green: 0, blue: 128/255.0, alpha: 0.5).cgColor
-borderLayer.borderWidth = 5
-borderLayer.borderColor = UIColor(red: 176/255.0, green: 196/255.0, blue: 222/255.0, alpha: 0.5).cgColor
-view.layer.addSublayer(borderLayer)
-
-let textLayer = CATextLayer()
-textLayer.string = "С днём рождения!"
-textLayer.frame = CGRect(x: 150, y: 150, width: 300, height: 100)
-textLayer.fontSize = 35
-textLayer.foregroundColor = UIColor(red: 255/255.0, green: 222/255.0, blue: 173/255.0, alpha: 1).cgColor
-textLayer.alignmentMode = .center
-view.layer.addSublayer(textLayer)
-
+ 
+// CALayer, CAGradientLayer
+// let textLayer = CATextLayer()
+/*
+ CATextLayer *label = [[CATextLayer alloc] init];
+ [label setFont:@"Helvetica-Bold"];
+ [label setFontSize:20];
+ [label setFrame:validFrame];
+ [label setString:@"Hello"];
+ [label setAlignmentMode:kCAAlignmentCenter];
+ [label setForegroundColor:[[UIColor whiteColor] CGColor]];
+ [layer addSublayer:label];
+ */
+ 
+let shapeLayer = CAShapeLayer()
+// shapeLayer.path
